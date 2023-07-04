93c7c0ddcf660795eb31a2f2e7ca607d10d8e90b
Maximo Guzman <vmaximoguzman@gmail.com>
2023-07-04 08:20:20 -0300

    Ajustando width & color nav + foot

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,38 +7,40 @@ import { AiOutlineSend } from "react-icons/ai";
 export const Footer = () => {
   return (
     <div className="footBar">
-      <div className="footNav">
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/directory">Directory</NavLink>
-          </li>
-          <li>
-            <NavLink to="/faq">FAQ</NavLink>
-          </li>
-          <li>
-            <NavLink to="/calendar">Calendar</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-      </div>
-      <div className="footInfo">
-        <div className="footTipo">
-          <img src={logotipo} alt="Logotipo" />
+      <div className="footWidth">
+        <div className="footNav">
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/directory">Directory</NavLink>
+            </li>
+            <li>
+              <NavLink to="/faq">FAQ</NavLink>
+            </li>
+            <li>
+              <NavLink to="/calendar">Calendar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
         </div>
-        <div className="footNews">
-          <p>Newsletter</p>
-          <form>
-            <button>
-              <AiOutlineSend />
-            </button>
-            <input type="email" name="" />
-            <label>Email</label>
-          </form>
+        <div className="footInfo">
+          <div className="footTipo">
+            <img src={logotipo} alt="Logotipo" />
+          </div>
+          <div className="footNews">
+            <p>Newsletter</p>
+            <form>
+              <button>
+                <AiOutlineSend />
+              </button>
+              <input type="email" name="" />
+              <label>Email</label>
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,24 +7,26 @@ import img from "../../images/MyPannelsLogo.png";
 export const Navbar = () => {
   return (
     <div className="divBar">
-      <div className="navLogo">
-        <NavLink to="/">
-          <img src={img} alt="MyPannels Logo" />
-        </NavLink>
-      </div>
-      <div className="navEgation">
-        <ul>
-          <li>
-            <NavLink to="/">
-              <BiHomeAlt2 />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/directory">
-              <BiSearch />
-            </NavLink>
-          </li>
-        </ul>
+      <div className="divWidth">
+        <div className="navLogo">
+          <NavLink to="/">
+            <img src={img} alt="MyPannels Logo" />
+          </NavLink>
+        </div>
+        <div className="navEgation">
+          <ul>
+            <li>
+              <NavLink to="/">
+                <BiHomeAlt2 />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/directory">
+                <BiSearch />
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
