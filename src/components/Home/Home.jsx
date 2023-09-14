a4f90968a3ba07aa30ca46dc4adbc43858f0ab78
Maximo Guzman <vmaximoguzman@gmail.com>
2023-09-14 12:45:10 -0300

    ..

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -19,10 +19,3 @@ export const Home = () => {
     </div>
   );
 };
-
-/* 
-*Cambiar TOP 3 por:
-  - Próximos Mangas (Proximamente?). Arreglamos problema de BD agregando parte en disponible que diga Pending (Sin stock, pending, available).
-  - Mangas más vendidos (TOP). Copia de la comiqueria, no me gusta esto, pero es mejor que nada.
-  - Recomendados(?). Problema con esto es que debería tener una base de datos para los usuarios donde guardar los generos más comprados, al igual que una interfaz para los usuarios (Creeme que sería medio muy incómodo ;-;).
-*/
