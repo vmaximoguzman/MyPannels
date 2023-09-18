f37c82b063970ca89b6855c917f9034e963e635e
Maximo Guzman <vmaximoguzman@gmail.com>
2023-09-18 12:45:05 -0300

    .

--- src/components/Directory/Search/Search.jsx
+++ src/components/Directory/Search/Search.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import "./Search.scss";
+
+import { AiOutlineSearch } from "react-icons/ai";
 
 export const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="divSearch">
+      <div className="filter">
+        <p>Filtros</p>
+      </div>
+
+      <div className="search">
+        <form action="">
+          <input type="text" />
+          <label>Buscar</label>
+          <button>
+            <AiOutlineSearch />
+          </button>
+        </form>
+      </div>
+
+      <div className="searchGenres"></div>
+    </div>
+  );
 };
